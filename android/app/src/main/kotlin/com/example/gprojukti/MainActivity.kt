e169a6ace5543d0587d1f8927b9feeb5a4b69455
Your Name <you@example.com>
2024-04-28 22:15:42 +0600

    GitHub add

---
+++ android/app/src/main/kotlin/com/example/gprojukti/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gprojukti
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Screen/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: Colors.red,
+        child: Center(
+          child: Text("Gpojukti"),
+        ),
+      ),
+    );
+  }
+}
