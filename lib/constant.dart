6119937bfbeeb186ffbca933078e5bed699faf16
Your Name <you@example.com>
2024-04-29 22:44:31 +0600

    custom Text and button add

---
+++ lib/constant.dart
@@ -0,0 +1,11 @@
+
+
+import 'dart:ui';
+ 
+const redColor=Color(0xffE83330); 
+const appbarColor=redColor;
+const custombuttoncollor=Color(0xffE83330); 
+const defaultBackgroundColor = Color(0xffe5e3df); 
+const buttonTextColor = Color(0xed24fa1c);
+
+const BaseUrl="http://ocadocourier.com";
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gprojukti/Screen/home_screen.dart';
+import 'package:gprojukti/View/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
